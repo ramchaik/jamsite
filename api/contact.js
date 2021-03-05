395dca52749fab432ce2c59a7ab89eba3904f8ef
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-05 10:27:01 +0530

    feat: add node http server

---
+++ api/contact.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+  if (req.method === 'POST') {
+
+    let body = '';
+    req.on('data', data => body += data)
+
+    req.on('end', () => {
+      console.log(body);
+
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      })
+      res.end('success');
+    })
+  }
+})
+
+const PORT = 2222;
+server.listen(PORT, () => {
+  console.log("Server listening on " + PORT);
+});
