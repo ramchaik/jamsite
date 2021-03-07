7e153fc33b0d264a052aa1c336f297f272ca53a0
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-03-07 15:05:20 +0530

    refactor: update contact handler

--- api/contact.js
+++ api/contact.js
@@ -1,22 +1,15 @@
-const http = require("http");
+module.exports = (req, res) => {
+  let body = "";
+  req.on("data", (data) => (body += data));
 
-const server = http.createServer((req, res) => {
-  if (req.method === "POST") {
-    let body = "";
-    req.on("data", (data) => (body += data));
+  req.on("end", () => {
+    console.log(body);
 
-    req.on("end", () => {
-      console.log(body);
-
-      res.writeHead(200, {
-        "Content-Type": "text/plain",
-      });
-      res.end("success");
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
     });
-  }
-});
+    res.end("success");
+    return;
+  });
+};
 
-const PORT = 2222;
-server.listen(PORT, () => {
-  console.log("Server listening on " + PORT);
-});
